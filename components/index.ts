b036fc69f221d3c816348615f488bf099543931c
sugampalav <sugampalav.dev@gmail.com>
2023-11-07 13:45:42 +0530

    build error fix

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,9 @@ import CarCard from "./CarCard";
 import CustomButton from "./CustomButton";
 import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
-import NavBar from "./Navbar";
+import NavBar from "./NavBar";
 import ShowMore from "./ShowMore";
-import SearchBar from "./Searchbar";
+import SearchBar from "./SearchBar";
 import Hero from "./Hero";
 
 export {
